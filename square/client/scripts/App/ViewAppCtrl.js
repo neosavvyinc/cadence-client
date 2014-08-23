ea63098a54efde0d0ad007ba749eab5510f4e2ec
Trevor Ewen <tewen@neosavvy.com>
2014-08-23 17:32:31 -0400

    The first integration for apps loaded up from the db.

--- square/client/scripts/App/ViewAppCtrl.js
+++ square/client/scripts/App/ViewAppCtrl.js
@@ -1,16 +1,15 @@
 angular.module('cadence.app.ctrls').controller('viewAppCtrl',
-    ['$scope', '$location', function ($scope, $location) {
-        $scope.app = {
-            id: 26,
-            name: 'Sneeky',
-            url: 'https://itunes.apple.com/us/app/sneeky/id797190314?mt=8',
-            type: 'iPhone',
-            market: 'Photo & Video',
-            apiKey: new Date().getTime()
-        };
-        $scope.$location = $location;
-        $scope.chartTypes = {
-            real: 'Real Time Chart',
-            line: 'Line Chart'
-        };
-    }]);
+    ['$scope', '$location', 'App', '$route',
+        function ($scope, $location, App, $route) {
+
+            //Load up the app
+            App.one($route.current.params.id).get().then(function (app) {
+                $scope.app = app;
+            });
+
+            $scope.$location = $location;
+            $scope.chartTypes = {
+                real: 'Real Time Chart',
+                line: 'Line Chart'
+            };
+        }]);
