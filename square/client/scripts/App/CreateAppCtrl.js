74fdbb336545ac713a4dc1a595b3f6ba5ac21aaf
Trevor Ewen <tewen@neosavvy.com>
2014-08-23 17:45:53 -0400

    Latest changes for create view to master.

--- square/client/scripts/App/CreateAppCtrl.js
+++ square/client/scripts/App/CreateAppCtrl.js
@@ -1,11 +1,18 @@
 angular.module('cadence.app.ctrls').controller('createAppCtrl',
-    ['$scope', 'MarketsData', 'App', function ($scope, markets, App) {
+    ['$scope', 'MarketsData', 'App', '$route', function ($scope, markets, App, $route) {
         $scope.appTypes = ["iPhone", "Android"];
+
+        if ($route.current.params.id) {
+            App.one($route.current.params.id).get().then(function (app) {
+                $scope.app = app;
+            });
+        } else {
+            $scope.app = { type: $scope.appTypes[0] };
+        }
+
         $scope.markets = markets;
-        $scope.app = { appType: $scope.appTypes[0] };
 
         $scope.submitForm = function () {
-            console.log('submit');
             App.post($scope.app);
         }
     }]);
